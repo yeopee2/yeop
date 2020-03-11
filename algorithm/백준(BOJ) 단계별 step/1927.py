d9c77e02e9c3342f41b8f29ef7e28dbe0b37af55
yeopee2 <pxht4320@naver.com>
2020-03-11 16:14:51 +0900

    Create 1927.py

---
+++ algorithm/백준(BOJ) 단계별 step/1927.py
@@ -0,0 +1,18 @@
+import sys
+import heapq as h
+
+N = int(sys.stdin.readline())
+heap = []
+
+for i in range(N):
+    x = int(sys.stdin.readline())
+
+    if x > 0:
+        h.heappush(heap,x)
+    
+    elif x == 0:
+        if len(heap) == 0:
+            print(0)
+        
+        else:
+            print(h.heappop(heap))
